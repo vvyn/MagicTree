b7f4a62ff963dd90e78bb371af0228f09cec9e0d
Riya Gandhi <griya2001@gmail.com>
2024-03-23 16:53:25 -0500

    Added Firebase Functionality-Riya

--- magictree/src/App.js
+++ magictree/src/App.js
@@ -1,26 +1,21 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import Navbar from "./components/NavBar";
+//import Navbar from "./components/NavBar";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {Login} from './pages/Login.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Navbar />
-      </header>
-    </div>
+    <>
+    {
+      <Router>
+        <Routes>
+            <Route exact path = "/" element = {<Login />}>/</Route>
+        </Routes>
+      </Router>
+    }
+    </>
   );
 }
 
---
+++ magictree/src/pages/Login.js
@@ -0,0 +1,47 @@
+import {signUpWithEmail, loginWithEmail } from "../Firebase.js";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from 'react';
+
+
+export function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const predefinedPassword = "set_pass"
+
+  function handleEmailSignUp(){
+    signUpWithEmail(email, predefinedPassword)
+      .then(() => {
+        alert("You have successfully been registered");
+        document.getElementById('registerEmail').value='';
+        console.log("Success!")
+      })
+      .catch(error =>{
+        console.error(error);
+      })
+  }
+
+  function handleEmailLogin(){
+    loginWithEmail(loginEmail, predefinedPassword)
+      .then(() => {
+        alert("You have successfully logged in");;
+      })
+      .catch(error =>{
+        console.error(error);
+      })
+  }
+  
+  return (
+    <>
+      <div className="center">
+       <input type="email" id="registerEmail" className="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+
+       <button onClick={handleEmailSignUp}>Sign Up</button>
+
+       <input type="email" id="registerEmail" className="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email" value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)}/>
+       <button onClick={handleEmailLogin}>Login</button>
+      </div>
+    </>
+  );
+}
