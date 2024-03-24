2e1fec394bde6352896e1559f7c5f08cc48f416e
roshni-bedd <roshni.beddhannan@gmail.com>
2024-03-23 23:01:48 -0500

    profile page

--- magictree/src/App.js
+++ magictree/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 //import Navbar from "./components/NavBar";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import {Login} from './pages/Login.js';
-import { Signup } from './pages/Signup.js';
+import { Profile } from './pages/Profile.js';
+import { ProfileMatch } from './pages/ProfileMatch.js';
 
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
       <Router>
         <Routes>
             <Route exact path = "/" element = {<Login />}>/</Route>
+            <Route exact path = "/Profile" element = {<Profile />}>/</Route>
+            <Route exact path = "/ProfileMatch" element = {<ProfileMatch />}>/</Route>
         </Routes>
       </Router>
     }
