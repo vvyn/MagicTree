5b1ffd7da81b1dab9767e0d7c5384556c4c73e58
Riya Gandhi <griya2001@gmail.com>
2024-03-23 19:38:42 -0500

    Basic Email Login

--- magictree/src/App.js
+++ magictree/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 //import Navbar from "./components/NavBar";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import {Login} from './pages/Login.js';
+import { Signup } from './pages/Signup.js';
 
 
 function App() {
--- magictree/src/pages/Login.js
+++ magictree/src/pages/Login.js
@@ -34,13 +34,26 @@ export function Login() {
   
   return (
     <>
-      <div className="center">
-       <input type="email" id="registerEmail" className="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+      <div className=" h-screen w-screen bg-[#CAC2AF]">
+        <div>
+        <div className="text-center py-2">
+            Already Have an Account: Login
+        </div>
+        <div className="px-4 py-2 justify-center text-center ">
+            <input type="email" id="registerEmail" className="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <div className="py-2">
+                <button onClick={handleEmailSignUp} className="bg-[#2A4223] hover:bg-blue-700 text-white font-bold w-full py-2 px-4 rounded">Sign Up</button>
+            </div>
+        </div>
+       
 
-       <button onClick={handleEmailSignUp}>Sign Up</button>
-
-       <input type="email" id="registerEmail" className="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email" value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)}/>
-       <button onClick={handleEmailLogin}>Login</button>
+        <div className="px-4 py-2 justify-center text-center ">
+           <input type="email" id="registerEmail" className="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email" value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)}/>
+           <div className="py-2">
+                <button onClick={handleEmailLogin} className="bg-[#2A4223] hover:bg-blue-700 text-white font-bold w-full py-2 px-4 rounded">Login</button> 
+           </div>
+        </div>
+       </div>
       </div>
     </>
   );
